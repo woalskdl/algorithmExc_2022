da7968e272ba12a90a62029af136786dbb16a319
jay <jay.wekeep@gmail.com>
2022-01-18 09:22:26 +0900

    2294 아오 집중안됨. 아무튼 시간초과 / 2차원 배열로 해결할 수 있을듯. dp 내부에서 for문 돌면 왠만하면 시간초과

--- src/Jan/P2294_my.java
+++ src/Jan/P2294_my.java
@@ -9,7 +9,7 @@ public class P2294_my {
 
     private static int n;
     private static int k;
-    private static int[] coins;
+    private static int[][] coins;
     private static int[] arr;
 
     public static void main(String[] args) throws IOException {
@@ -19,39 +19,40 @@ public class P2294_my {
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
-        coins = new int[n];
-        arr = new int[k + 1];
+        coins = new int[n][k + 1];
+//        arr = new int[k + 1];
 
-        for(int i=0; i<=k; i++)
-            arr[i] = -1;
+        for(int i=0; i<n; i++)
+            for(int j=0; j<=k; j++)
+                coins[i][j] = -1;
 
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int val = Integer.parseInt(st.nextToken());
-            coins[i] = val;
-            arr[val] = 1;
+            coins[i][0] = val;
+
+            if(val <= k)
+                coins[i][val] = 1;
         }
 
         System.out.println(dp(k));
     }
 
-    private static int dp(int val){
-        if(val <= 0)
+    private static int dp(int val) {
+        if (val <= 0)
             return -1;
 
-        if(arr[val] != -1)
+        if (arr[val] != -1)
             return arr[val];
 
-        // ArrayIndexOutOfBound???
-
         int minVal = Integer.MAX_VALUE;
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int count = dp(val - coins[i]) + 1;
-            if(count != 0 && minVal > count)
+            if (count != 0 && minVal > count)
                 minVal = count;
         }
 
-        if(minVal == Integer.MAX_VALUE)
+        if (minVal == Integer.MAX_VALUE)
             minVal = -1;
 
         return arr[val] = minVal;
