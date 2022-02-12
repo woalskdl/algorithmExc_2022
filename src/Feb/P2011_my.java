36a2f5d20fa4d08240fd2cb10c66b8d1f82a9cc7
wooljam <woalskdl@gmail.com>
2022-02-12 15:38:04 +0900

    2011 집에선 머리가 잘 안돌아가나보다

---
+++ src/Feb/P2011_my.java
@@ -0,0 +1,84 @@
+package Feb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class P2011_my {
+
+    private static final int MAX = 26;
+    private static final int REM = 1000000;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String codeLine = br.readLine();
+        if(codeLine == null || codeLine.length() == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        int length = codeLine.length();
+
+        int[] code = new int[length];
+        int[] dp = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            code[i] = codeLine.charAt(i) - '0';
+
+            if(code[i] < 0 || code[i] > 9) {
+                System.out.println(0);
+                return;
+            }
+        }
+
+        if (code[0] == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        dp[0] = 1;
+
+        StringBuilder sb;
+
+        for (int i = 1; i < length; i++) {
+            sb = new StringBuilder();
+
+            sb.append(code[i - 1]);
+            sb.append(code[i]);
+
+            int num = Integer.parseInt(String.valueOf(sb));
+
+            if (num <= 0)
+                break;
+
+            if (num <= MAX) {
+                if(num % 10 == 0) {
+                    if (i == 1) {
+                        dp[i] = dp[i - 1];
+                        continue;
+                    }
+
+                    dp[i] = dp[i - 1] == dp[i - 2] ? dp[i - 1] : dp[i - 1] - 1;
+                } else if (num < 10) {
+                    dp[i] = dp[i - 1];
+                } else {
+                    if(i == 1) {
+                        dp[i] = (dp[i - 1] + 1) % REM;
+                        continue;
+                    }
+
+                    dp[i] = (dp[i - 1] % REM + dp[i - 2] % REM) % REM;
+                }
+            } else {
+                if (num % 10 == 0)
+                    break;
+
+                dp[i] = dp[i - 1];
+            }
+        }
+
+        System.out.println(dp[length - 1]);
+    }
+}
