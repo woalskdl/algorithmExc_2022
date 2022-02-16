eea15f4b5962ec2a13957da93940bed4fd941afc
jay <jay.wekeep@gmail.com>
2022-02-16 09:29:50 +0900

    1094 / 11723 >> for 문 안에서의 System.out.println 은 시간을 상당히 잡아먹는다

---
+++ src/Feb/P1094_answer.java
@@ -0,0 +1,22 @@
+package Feb;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 비트마스킹 문제 풀어보려고 이 문제 풀었기 때문에, 비트마스킹 접근 풀이
+public class P1094_answer {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int result = 0;
+
+        for(int i=0; i<7; i++) {
+            if((n & (1 << i)) > 0)
+                result += 1;
+        }
+
+        System.out.println(result);
+    }
+}
