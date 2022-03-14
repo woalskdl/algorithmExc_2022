32e72033a9be31d6d7db23dccf1d98eca1405832
jay <jay.wekeep@gmail.com>
2022-03-14 09:17:13 +0900

    1086 집중이 겁나 안되네. 해설 보고 이해해보자

---
+++ src/Mar/P1086_answer.java
@@ -0,0 +1,25 @@
+package Mar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1086_answer {
+    private static int N, K;
+    private static String[] num;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        num = new String[N];
+
+        for (int i = 0; i < N; i++)
+            num[i] = br.readLine();
+
+        K = Integer.parseInt(br.readLine());
+
+
+    }
+}
