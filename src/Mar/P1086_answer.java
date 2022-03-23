72371e1100ad7304ab0ba8f64aca40132b8f310d
jay <jay.wekeep@gmail.com>
2022-03-23 09:36:34 +0900

    1086 좀더 이해해보자

--- src/Mar/P1086_answer.java
+++ src/Mar/P1086_answer.java
@@ -3,13 +3,19 @@ package Mar;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class P1086_answer {
+
     private static int N, K;
     private static char[][] num;
 
-    private static int cases;
-    private static int total;
+    private static long[] fibo;
+    private static long[][] dp;
+    private static int[][] dpMod;
+
+    private static long p;
+    private static long q;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -23,48 +29,81 @@ public class P1086_answer {
 
         K = Integer.parseInt(br.readLine());
 
-        dp(0, 0);
+        fibo = new long[N + 1];
+        fibo[1] = 1L;
+        for(int i=2; i<=N; i++)
+            fibo[i] = (long)i * fibo[i - 1];
 
-        String result;
+        dp = new long[K][1 << N];
+        dpMod = new int[K][N];
 
-        if(cases == 0)
-            result = "0/1";
-        else if (cases == total)
-            result = "1/1";
-        else
-            result = cases + "/" + total;
+        for(int i=0; i<K; i++) {
+            Arrays.fill(dp[i], -1);
+            Arrays.fill(dpMod[i], -1);
+        }
 
-        System.out.println(result);
+        p = memo(0, 0, 0);
+        q = fibo[N];
+
+        if(p == 0) {
+            q = 1;
+        } else if (p == q) {
+          p = 1;
+          q = 1;
+        } else {
+            long gcd = GCD(p, q);
+            p /= gcd;
+            q /= gcd;
+        }
+
+        System.out.println(p + "/" + q);
 
     }
 
-    private static void dp(int flag, int r) {
-        if(((1 << N) - 1) - flag == 0) {
-            if(r == 0)
-                cases += 1;
+    private static long memo(int mod, int cnt, int flag) {
+        if(dp[mod][flag] != -1)
+            return dp[mod][flag];
 
-            total += 1;
-            return;
+        if(cnt == N)
+            return dp[mod][flag] = mod == 0 ? 1L : 0;
+
+        long sum = 0;
+        for(int i=0; i<N; i++) {
+            if((flag & (1 << i)) == (1 << i))
+                continue;
+
+            sum += memo(getMod(mod, i), cnt + 1, flag | (1 << i));
         }
 
-        String rt = Integer.toString(r);
+        return dp[mod][flag] = sum;
+    }
 
-        for(int i=0, length = num.length; i<length; i++) {
-            if(((1 << i) & flag) == 0) {
-                for(int j=0, size = num[i].length; j<size; j++) {
-                    String temp = rt + num[i][j];
-                    int n = Integer.parseInt(temp);
+    private static int getMod(int mod, int n) {
+        if(dpMod[mod][n] != -1)
+            return dpMod[mod][n];
 
-                    System.out.println("n >> " + n);
-                    System.out.println("n % K >> " + (n % K));
-
-                    rt = Integer.toString(n % K);
-
-                    System.out.println("rt >> " + rt);
-                }
-
-                dp(flag | (1 << i), Integer.parseInt(rt));
-            }
+        int cur = mod;
+        for(int i=0, length = num[n].length; i<length; i++) {
+            cur *= 10;
+            cur = (cur + num[n][i] - '0') % K;
         }
+
+        return dpMod[mod][n] = cur;
+    }
+
+    private static long GCD(long p, long q) {
+        if(p > q) {
+            long tmp = p;
+            p = q;
+            q = tmp;
+        }
+
+        while (p % q != 0) {
+            long tmp = p % q;
+            p = q;
+            q = tmp;
+        }
+
+        return q;
     }
 }
---
+++ src/Mar/P1086_my2.java
@@ -0,0 +1,66 @@
+package Mar;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class P1086_my2 {
+    private static int N, K;
+    private static char[][] num;
+
+    private static int cases;
+    private static int total;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        num = new char[N][];
+
+        for (int i = 0; i < N; i++)
+            num[i] = br.readLine().toCharArray();
+
+        K = Integer.parseInt(br.readLine());
+
+        dp(0, 0);
+
+        String result;
+
+        if(cases == 0)
+            result = "0/1";
+        else if (cases == total)
+            result = "1/1";
+        else
+            result = cases + "/" + total;
+
+        System.out.println(result);
+
+    }
+
+    private static void dp(int flag, int r) {
+        if(((1 << N) - 1) - flag == 0) {
+            if(r == 0)
+                cases += 1;
+
+            total += 1;
+            return;
+        }
+
+        String rt = Integer.toString(r);
+
+        for(int i=0, length = num.length; i<length; i++) {
+            if(((1 << i) & flag) != 0)
+                continue;
+
+            for(int j=0, size = num[i].length; j<size; j++) {
+                String temp = rt + num[i][j];
+                int n = Integer.parseInt(temp);
+
+                rt = Integer.toString(n % K);
+            }
+
+            dp(flag | (1 << i), Integer.parseInt(rt));
+        }
+    }
+}
