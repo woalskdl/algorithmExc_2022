1bea5d487ea5084e9650d31869c478d1460331a5
jay <jay.wekeep@gmail.com>
2022-03-30 08:56:42 +0900

    1029 3차원 배열을 처음 써봤는데, 되네 ㅎ

--- src/Mar/P1029_my.java
+++ src/Mar/P1029_my.java
@@ -8,19 +8,23 @@ import java.util.Arrays;
 public class P1029_my {
     private static int N;
     private static int[][] price;
-    private static int[][] dp;
+    private static int[][][] dp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         price = new int[N][N];
-        dp = new int[N][1 << N];
+        dp = new int[N][10][1 << N];
 
         for (int i = 0; i < N; i++) {
             String line = br.readLine();
-            Arrays.fill(dp[i], -1);
-            for (int j = 0; j < N; j++)
+            for (int j = 0; j < N; j++) {
                 price[i][j] = line.charAt(j) - '0';
+            }
+        }
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<10; j++)
+                Arrays.fill(dp[i][j], -1);
         }
 
         System.out.println(memo(0, 0, 1, 1));
@@ -30,8 +34,8 @@ public class P1029_my {
         if (flag == (1 << N) - 1)
             return N;
 
-        if(dp[n][flag] != -1)
-            return Math.max(cnt, dp[n][flag]);
+        if(dp[n][pay][flag] != -1)
+            return dp[n][pay][flag];
 
         int result = cnt;
         for (int i = 0; i < N; i++) {
@@ -41,6 +45,6 @@ public class P1029_my {
             result = Math.max(result, memo(i, price[n][i], cnt + 1,flag | (1 << i)));
         }
 
-        return dp[n][flag] = result;
+        return dp[n][pay][flag] = result;
     }
 }
