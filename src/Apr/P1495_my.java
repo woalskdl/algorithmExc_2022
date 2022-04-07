5e42a51726b2415a9dce08338d410786fee4fec8
jay <jay.wekeep@gmail.com>
2022-04-07 09:35:46 +0900

    1495 메모리 초과 하

--- src/Apr/P1495_my.java
+++ src/Apr/P1495_my.java
@@ -3,7 +3,9 @@ package Apr;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class P1495_my {
@@ -22,29 +24,48 @@ public class P1495_my {
         M = Integer.parseInt(st.nextToken());
 
         V = new int[N];
-        dp = new int[N][(int) Math.pow(2, N)];
+        List<Integer> dp = new ArrayList<>();
 
         st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++)
             V[i] = Integer.parseInt(st.nextToken());
-            Arrays.fill(dp[i], -1);
-        }
 
-        dp[0][0] = S - V[0] >= 0 ? S - V[0] : -1;
-        dp[0][1] = S + V[0] <= M ? S + V[0] : -1;
+        dp.add(S);
+        List<Integer> list = new ArrayList<>();
 
-        for (int i = 1; i < N; i++) {
-            for (int j = 0, length = dp[i].length; j < length; j++) {
-                if (dp[i - 1][j] == -1)
-                    continue;
+        for (int i = 0; i < N; i++) {
+            list.clear();
 
-                dp[i][j * 2] = dp[i - 1][j] - V[i] >= 0 ? dp[i - 1][j] - V[i] : -1;
-                dp[i][j * 2 + 1] = dp[i - 1][j] + V[i] <= M ? dp[i - 1][j] + V[i] : -1;
+            for (int num : dp) {
+                if (num - V[i] >= 0)
+                    list.add(num - V[i]);
+
+                if (num + V[i] <= M)
+                    list.add(num + V[i]);
             }
+
+            dp.clear();
+            dp.addAll(list);
+
+//            int length = dp[i - 1].length * 2;
+//            dp[i] = new int[length];
+//
+//            for (int j = 0; j < length; j++) {
+//                if (dp[i - 1][j] == -1)
+//                    continue;
+//
+//                dp[i][j * 2] = dp[i - 1][j] - V[i] >= 0 ? dp[i - 1][j] - V[i] : -1;
+//                dp[i][j * 2 + 1] = dp[i - 1][j] + V[i] <= M ? dp[i - 1][j] + V[i] : -1;
+//            }
         }
 
 //        System.out.println(memo(0, S));
-        System.out.println(Arrays.stream(dp[N - 1]).max().getAsInt());
+//        System.out.println(Arrays.stream(dp[N - 1]).max().getAsInt());
+
+        if (dp.isEmpty())
+            System.out.println(-1);
+        else
+            System.out.println(Collections.max(dp));
 
     }
 
