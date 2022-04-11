e0cac3946121a70b6d75237ea9bb4319a54ab847
jay <jay.wekeep@gmail.com>
2022-04-11 09:24:14 +0900

    1495 오예 기분좋게 통과 ~_~

--- src/Apr/P1495_my.java
+++ src/Apr/P1495_my.java
@@ -21,76 +21,39 @@ public class P1495_my {
         M = Integer.parseInt(st.nextToken());
 
         V = new int[N];
-        List<Integer> dp = new ArrayList<>();
+        dp = new int[N + 1][M + 1];
 
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; i++)
             V[i] = Integer.parseInt(st.nextToken());
 
-        // V[i] 정렬한 다음에 싹 더해보고 크면 작은거부터 빼다가 M보다 작거나 같아지면 출력해보자
-        // M보다 작거나 같아지는 케이스가 여러 경우가 있을 경우, M보다 작아지려면 여러개의 수가 필요할 경우
-        Arrays.sort(V);
-        int result = S + Arrays.stream(V).sum();
+        for(int i=0; i<=N; i++)
+            Arrays.fill(dp[i], -1);
 
-        for(int i=0; i<N; i++) {
-            if(result <= M)
-                break;
-
-            result -= V[i] * 2;
-        }
-
-        System.out.println(result);
-
-        /*
-        dp.add(S);
-        List<Integer> list = new ArrayList<>();
+        dp[0][S] = 1;
 
         for (int i = 0; i < N; i++) {
-            list.clear();
+            for (int j = 0; j <= M; j++) {
+                if (dp[i][j] == -1)
+                    continue;
 
-            for (int num : dp) {
-                if (num - V[i] >= 0)
-                    list.add(num - V[i]);
+                int pls = j + V[i];
+                int min = j - V[i];
 
-                if (num + V[i] <= M)
-                    list.add(num + V[i]);
+                if (pls <= M)
+                    dp[i + 1][pls] = 1;
+                if (min >= 0)
+                    dp[i + 1][min] = 1;
             }
-
-            dp.clear();
-            dp.addAll(list);
-
-//            int length = dp[i - 1].length * 2;
-//            dp[i] = new int[length];
-//
-//            for (int j = 0; j < length; j++) {
-//                if (dp[i - 1][j] == -1)
-//                    continue;
-//
-//                dp[i][j * 2] = dp[i - 1][j] - V[i] >= 0 ? dp[i - 1][j] - V[i] : -1;
-//                dp[i][j * 2 + 1] = dp[i - 1][j] + V[i] <= M ? dp[i - 1][j] + V[i] : -1;
-//            }
         }
-         */
 
-//        System.out.println(memo(0, S));
-//        System.out.println(Arrays.stream(dp[N - 1]).max().getAsInt());
+        for (int i = M; i >= 0; i--){
+            if(dp[N][i] != -1) {
+                System.out.println(i);
+                return;
+            }
+        }
 
-        /*
-        if (dp.isEmpty())
-            System.out.println(-1);
-        else
-            System.out.println(Collections.max(dp));
-         */
-
-    }
-
-    private static int memo(int i, int P) {
-        if (P < 0 || P > M)
-            return -1;
-
-        if (i == N)
-            return P;
-
-        return Math.max(memo(i + 1, P - V[i]), memo(i + 1, P + V[i]));
+        System.out.println(-1);
     }
 }
